41647a97cbaea50a1c97fb74ab2c3574963341ce
Manar Ben salah <mben-sal@e1r9p7.1337.ma>
2022-10-06 22:14:07 +0100

    add functions to my project

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 19:45:18 by mben-sal          #+#    #+#             */
+/*   Updated: 2022/10/06 21:34:22 by mben-sal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+
+int ft_isalpha(int arg);
+int ft_isdigit( int arg );
+
+
+#endif
