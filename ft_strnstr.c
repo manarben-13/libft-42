45462d66063ad7ba8caac343bc9d4d595cc0c876
mben-sal <manarbensalah7@gmail.com>
2022-10-17 18:33:43 +0100

    push

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 12:16:18 by mben-sal          #+#    #+#             */
-/*   Updated: 2022/10/14 12:29:02 by mben-sal         ###   ########.fr       */
+/*   Updated: 2022/10/16 11:33:45 by mben-sal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,45 @@
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    unsigned char *str1;
-    unsigned char *str2;
-    size_t i ;
-    size_t j;
-    
-    str1 = (unsigned char*)haystack;
-    str2 = (unsigned char*)needle;
-    i = 0;
-
-    if(!str2)
-        return(str1);
-    while(str1[i]&& i < len )
-    {
-        j = 0;
-        while (str1[i]&& str2[j]&& str1[i] == str2[j])
-        {
-            i++;
-            j++;
-        }
-        if(str2[j] == '\0');
-        	return(str1[j-i]);
-        j = j - i + 1;
-    }
-  return(0);
+	char *str1;
+	char *str2;
+	size_t i;
+	size_t j;
+	
+	i = 0;
+	str1 = (char*)haystack;
+	str2 = (char*)needle;
+	
+	if (!str2)
+	{
+		return(str1);
+	}
+	while (i< len)
+	{
+		j = 0;
+		while (str1[i+j] == str2[j])
+		{
+			if(!str2[j+1])
+			{
+				return(str1 +i);
+			}
+			else
+			{
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
 }
 
-
-int main()
+int main ()
 {
-    char *m ="mery manar 1337";
-    char *n = "manar";
-    char *ptr = ft_strnstr(m,n,12);
-    printf("%s", ptr );
+	char *m;
+	char *n;
+	
+	m = "manar 1337 ben salah";
+	n = "";
+
+	printf("%s", ft_strnstr(m,n,12));
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,34 +6,61 @@
 /*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 16:43:02 by mben-sal          #+#    #+#             */
-/*   Updated: 2022/10/14 17:34:36 by mben-sal         ###   ########.fr       */
+/*   Updated: 2022/10/16 19:29:27 by mben-sal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char *ft_set (char *s1 , char *set )
+{
+  int i;
+  i = 0;
+  
+  while (s1[i] && set [i])
+  {
+    if(s1[i] == set[i])
+    {
+      s1[i];
+    }
+    else 
+    {
+      i++;
+    }
+    return(s1);
+  }
+}
+
 char *ft_strtrim(char const *s1, char const *set)
 {
-	char *ptr;
-	unsigned char *str;
-	unsigned char *s
-	int len;
-	int i;
-	
-	len = ft_strlen(s1);
-	i = 0;
-	str = (unsigned char*)s1;
-	s = (unsigned char*)set;
+    char *ptr;
+    unsigned char *str;
+    int len;
+    int i;
+    
+    i = 0;
+    str = ft_set (s1, set);
+    len = ft_strlen(str);
     ptr = (char*)malloc(len*sizeof(char) + 1);
-	if(!ptr)
-		return(NULL);
-	
+    if(!ptr)
+        return(NULL);
+        while (str[i] )
+        {
+          ptr[i] = str[i];
+          i++;
+        }
+        return(ptr);
+}
+    
 }
 int main ()
 {
-	char *m;
-	char *r;
-	m = "manar123";
-	r = "na";
-	printf("%s",ft_strtrim(m,r));
+    char *m;
+    char *r;
+    char *n;
+    m = "manar123";
+    r = "na";
+    n = ft_strtrim(m,r);
+    printf("%s", n);
+
 }
