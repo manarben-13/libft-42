8e759b8a5c651f9224f2700aa45ae4bbc29b83d6
mben-sal <manarbensalah7@gmail.com>
2022-11-06 21:41:48 +0100

    push

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 14:11:54 by mben-sal          #+#    #+#             */
-/*   Updated: 2022/11/06 21:26:21 by mben-sal         ###   ########.fr       */
+/*   Updated: 2022/11/06 21:36:40 by mben-sal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ void	*ft_memcpy(void *dest, const void *src, size_t	n)
 	return (dest);
 }
 
-int main ()
-{
-	char *n;// = "test basic du memcpy !";
-	// char src[22];
-	// char dst[22];
-	// char *s;
-	// s = memcpy("         ", ((void *)0), 3);
-	n = memcpy("NULL", NULL, -1);
-	printf("%s\n", n);
-}
+// int main ()
+// {
+// 	char *n;// = "test basic du memcpy !";
+// 	// char src[22];
+// 	// char dst[22];
+// 	// char *s;
+// 	// s = memcpy("         ", ((void *)0), 3);
+// 	n = memcpy("ahmed", "ahmed", -1);
+// 	printf("%s\n", n);
+// }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 14:38:43 by mben-sal          #+#    #+#             */
-/*   Updated: 2022/11/06 21:27:02 by mben-sal         ###   ########.fr       */
+/*   Updated: 2022/11/06 21:38:35 by mben-sal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*s;
 	size_t			i;
 
-	if (dest == src)
+	if (!dest && !src)
 		return (NULL);
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
@@ -35,12 +35,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	return (d);
 }
 
-int main ()
-{
-    char n [] = "1337 is a school";
-    char *s;
-    s = ft_memmove(n , n +3, 3);
-    printf("%s\n", s );
-	s = memmove(n , n + 3 , 3);
-    printf("%s", s );
-}
+// int main ()
+// {
+//     char n [] = "1337 is a school";
+//     char *s;
+//     s = ft_memmove(n , n +3, 3);
+//     printf("%s\n", s );
+// 	s = memmove(n , n + 3 , 3);
+//     printf("%s", s );
+// }
